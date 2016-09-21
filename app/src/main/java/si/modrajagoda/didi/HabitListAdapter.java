ac5948fe9b1cac24b8187ce7198d77dbeb269360
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2016-09-21 10:50:21 +0300

    [HT-20] Added new layout for the list of habits
    The layout for the list of cards was added, habit_list.xml. The fragment HabitListFragment was added, in order to substitute Activity's container with fragment layout. The layout habit_card was added as an element of habits' list. The adapter HabitListAdapter for the list of habits was added, with dummy data in it.

---
+++ app/src/main/java/si/modrajagoda/didi/HabitListAdapter.java
@@ -0,0 +1,50 @@
+package si.modrajagoda.didi;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import si.modrajagoda.didi.database.Habit;
+
+/**
+ * Created by Nikolay Yushkevich on 21.09.16.
+ */
+public class HabitListAdapter extends RecyclerView.Adapter<HabitListAdapter.ViewHolder> {
+
+    private List<Habit> habits;
+    private String[] dummyList = new String[]{"Test1", "Test2", "Test3", "Test4", "Test5"};
+
+    /*public HabitListAdapter(List<Habit> habits) {
+        this.habits = habits;
+    }*/
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.habit_card, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String record = dummyList[position];
+        holder.title.setText(record);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dummyList.length;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.habit_name);
+        }
+    }
+}
--- app/src/main/java/si/modrajagoda/didi/Main.java
+++ app/src/main/java/si/modrajagoda/didi/Main.java
@@ -1,7 +1,10 @@
 package si.modrajagoda.didi;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,30 +15,54 @@ import android.view.MenuItem;
 
 public class Main extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private FragmentManager fragmentManager;
 
-    DrawerLayout drawer;
-
+    /**
+     * <b>onCreate</b> is invoked when Activity is first created (and not visible yet to the user)
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        fragmentManager = getSupportFragmentManager();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+
+        fragmentManager.beginTransaction().replace(R.id.container, new HabitListFragment()).commit();
+    }
+
+    /**
+     * <b>onStart</b> is invoked before Activity is visible to the user
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
+        //TODO: check if this item was selected before
+
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
         switch (item.getItemId()) {
             case R.id.nav_today:
+                fragment = new HabitListFragment();
                 break;
             case R.id.nav_all_tasks:
                 break;
@@ -43,8 +70,33 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
                 Log.d("Drawer", "Any other was clicked");
                 break;
         }
+
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+
+        //Set the item as checked in drawer menu
+        item.setChecked(true);
+
+        // Set action bar title
+        setTitle(item.getTitle());
+
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE! Make sure to override the method with only a single `Bundle` argument
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        toggle.onConfigurationChanged(newConfig);
     }
 
 }
