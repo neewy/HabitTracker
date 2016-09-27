f3261dcf7762171211321aba1d3de1b0a88237d4
Bulat Mukhutdinov <Bulatmm@yandex.ru>
2016-09-27 13:49:22 +0300

    [HT-20] Add habit page

---
+++ app/src/main/java/si/modrajagoda/didi/activity/AddHabitActivity.java
@@ -0,0 +1,19 @@
+package si.modrajagoda.didi.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import si.modrajagoda.didi.R;
+
+/**
+ * Created by Bulat Mukhutdinov on 24.09.2016.
+ */
+public class AddHabitActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_habit);
+    }
+}
