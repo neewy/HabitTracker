f80a15eb014be9381a480c8b13a195ed6ba69320
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-09-29 00:32:07 +0300

    HT-33, HT-35, HT-36 Modified DataAccessObjectTest class (now it is possible to run DataAccessObjectTest if the DB is not empty)

--- app/src/androidTest/java/si/modrajagoda/didi/DataAccessObjectsTest.java
+++ app/src/androidTest/java/si/modrajagoda/didi/DataAccessObjectsTest.java
@@ -3,13 +3,19 @@ package si.modrajagoda.didi;
 import android.os.Environment;
 import android.test.AndroidTestCase;
 
+import com.j256.ormlite.stmt.QueryBuilder;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import si.modrajagoda.didi.db.Constants;
+import si.modrajagoda.didi.db.DatabaseHelper;
+import si.modrajagoda.didi.db.DatabaseManager;
 import si.modrajagoda.didi.db.dataaccessobjects.HabitCategoryDAO;
 import si.modrajagoda.didi.db.dataaccessobjects.HabitDAO;
 import si.modrajagoda.didi.db.dataaccessobjects.HabitScheduleDAO;
@@ -22,6 +28,7 @@ import si.modrajagoda.didi.db.tablesrepresentations.HabitSchedule;
  */
 
 public class DataAccessObjectsTest extends AndroidTestCase {
+    private DatabaseHelper helper;
     private int id;
     private String modifiedDateTime;
     private String name;
@@ -37,8 +44,9 @@ public class DataAccessObjectsTest extends AndroidTestCase {
 
     @Before
     public void setUp() throws Exception {
-        // TODO: increment DATABASE_VERSION in db/Constants.java or delete current db version
-        // through adb shell before running the test
+        DatabaseManager.setHelper(this.getContext());
+        helper = DatabaseManager.getHelper();
+
         id = 1;
         modifiedDateTime = "2015-01-02 03:04:05.6";
         name = "sleep";
@@ -62,6 +70,21 @@ public class DataAccessObjectsTest extends AndroidTestCase {
     @Test
     public void testWritingNewHabitDataToMobileDB() throws ParseException {
         HabitDAO habitDAO = new HabitDAO(this.getContext());
+
+        /* if table is not empty, id = maxIdInTheTable+1, else id=1 */
+        QueryBuilder<Habit, Integer> qBuilder;
+        try {
+            qBuilder = helper.getHabitDao().queryBuilder();
+            if (qBuilder.query().size() > 0) {
+                qBuilder.orderBy(Constants.ID, false); // false for descending order
+                qBuilder.limit(1);
+                id = qBuilder.query().get(0).getId() + 1;
+            } else {
+                id = 1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         Habit habitWithTheSameId;
         Habit habitFromMobileDatabaseWithMaxId;
@@ -124,6 +147,21 @@ public class DataAccessObjectsTest extends AndroidTestCase {
     public void testWritingNewHabitCategoryDataToMobileDB() throws ParseException {
         HabitCategoryDAO habitCategoryDAO = new HabitCategoryDAO(this.getContext());
 
+        /* if table is not empty, id = maxIdInTheTable+1, else id=1 */
+        QueryBuilder<HabitCategory, Integer> qBuilder;
+        try {
+            qBuilder = helper.getHabitCategoryDao().queryBuilder();
+            if (qBuilder.query().size() > 0) {
+                qBuilder.orderBy(Constants.ID, false); // false for descending order
+                qBuilder.limit(1);
+                id = qBuilder.query().get(0).getId() + 1;
+            } else {
+                id = 1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         HabitCategory habitCategoryWithTheSameId;
         HabitCategory habitCategoryFromMobileDatabaseWithMaxId;
 
@@ -181,6 +219,21 @@ public class DataAccessObjectsTest extends AndroidTestCase {
     @Test
     public void testWritingNewHabitScheduleDataToMobileDB() throws ParseException {
         HabitScheduleDAO habitScheduleDAO = new HabitScheduleDAO(this.getContext());
+
+        /* if table is not empty, id = maxIdInTheTable+1, else id=1 */
+        QueryBuilder<HabitSchedule, Integer> qBuilder;
+        try {
+            qBuilder = helper.getHabitScheduleDao().queryBuilder();
+            if (qBuilder.query().size() > 0) {
+                qBuilder.orderBy(Constants.ID, false); // false for descending order
+                qBuilder.limit(1);
+                id = qBuilder.query().get(0).getId() + 1;
+            } else {
+                id = 1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         HabitSchedule habitScheduleWithTheSameId;
         HabitSchedule habitScheduleFromMobileDatabaseWithMaxId;
