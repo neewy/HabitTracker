8b17baa02aa5286d261646d0c7b48b57601d266a
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2016-10-04 10:44:09 +0300

    HT-37 Created XML layout for statistics (habit view)

--- app/src/main/java/ru/android4life/habittracker/fragment/HabitStatisticsFragment.java
+++ app/src/main/java/ru/android4life/habittracker/fragment/HabitStatisticsFragment.java
@@ -8,7 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-import az.plainpie.PieView;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.android4life.habittracker.R;
 
 /**
@@ -21,16 +28,37 @@ import ru.android4life.habittracker.R;
 public class HabitStatisticsFragment extends Fragment {
 
     private RelativeLayout view;
+    LineChart chart;
 
-    // Pie View — https://github.com/zurche/plain-pie
-    private PieView pieView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = (RelativeLayout) inflater.inflate(R.layout.habit_statistics, container, false);
-        pieView = (PieView) view.findViewById(R.id.pie_view);
-        return view;
+        chart = (LineChart) view.findViewById(R.id.chart);
+        float[][] data = new float[][]{{0,1,2,3,4,5,6,7,8},{1f/3f,2f/3f,1f,1f/5f,2f/6f,1f,1f,0f,2f/3f}};
+        List<Entry> entries = new ArrayList<Entry>();
 
+        for (int i = 0; i < data[0].length; i++) {
+            entries.add(new Entry(data[0][i], data[1][i]));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setColor(R.color.colorPrimary);
+        dataSet.setValueTextColor(R.color.colorAccent);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.setDrawBorders(false);
+        //chart.setDrawGridBackground(false);
+        chart.setGridBackgroundColor(R.color.transparent);
+        chart.invalidate();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        chart.invalidate();
     }
 }
