4cf372f538ddc50ded9c44a283bfc3a3aff6fcec
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2016-11-06 20:52:23 +0300

    HT-90 Added map to habit parameters

--- app/src/main/java/ru/android4life/habittracker/activity/MapsActivity.java
+++ app/src/main/java/ru/android4life/habittracker/activity/MapsActivity.java
@@ -1,48 +1,120 @@
 package ru.android4life.habittracker.activity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.etiennelawlor.discreteslider.library.ui.DiscreteSlider;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import ru.android4life.habittracker.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends BaseActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private DiscreteSlider discreteSlider;
+    private Marker currentMarker;
+    private Circle currentCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        discreteSlider = (DiscreteSlider) findViewById(R.id.range_picker);
+        discreteSlider.setOnDiscreteSliderChangeListener(new DiscreteSlider.OnDiscreteSliderChangeListener() {
+            @Override
+            public void onPositionChanged(int position) {
+                setRange(position); //changes the circle radius if not null
+            }
+        });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                if (currentMarker != null) currentMarker.remove();
+                currentMarker = mMap.addMarker(new MarkerOptions().position(latLng));
+                setRange(discreteSlider.getPosition());
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(final Marker marker) {
+                AlertDialog.Builder markerDialogBuilder = new AlertDialog.Builder(MapsActivity.this);
+                markerDialogBuilder.setTitle(R.string.marker_delete);
+                markerDialogBuilder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        marker.remove();
+                        dialog.cancel();
+                    }
+                });
+                markerDialogBuilder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog markerDialog = markerDialogBuilder.create();
+
+                markerDialog.show();
+                return true;
+            }
+        });
     }
+
+    private void addCircle(int meters) {
+        if (currentMarker != null) {
+            CircleOptions circleOptions = new CircleOptions()
+                    .center(currentMarker.getPosition())
+                    .radius(meters)
+                    .strokeColor(Color.BLUE)
+                    .fillColor(Color.argb(100, 88, 87, 255));
+
+            if (currentCircle != null) {
+                currentCircle.remove();
+            }
+
+            currentCircle = mMap.addCircle(circleOptions);
+        }
+    }
+
+    private void setRange(int position) {
+        switch (position) {
+            case 0:
+                addCircle(1);
+                break;
+            case 1:
+                addCircle(10);
+                break;
+            case 2:
+                addCircle(50);
+                break;
+            case 3:
+                addCircle(100);
+                break;
+            case 4:
+                addCircle(500);
+                break;
+        }
+    }
+
 }
