07fc7b099fd69976d3c28e0716376d3592fe39c3
Bulat Mukhutdinov <Bulatmm@yandex.ru>
2016-11-18 18:32:23 +0300

    [HT-111] Redesign "Habits" fragment

--- app/src/main/java/ru/android4life/habittracker/adapter/HabitNotesAdapter.java
+++ app/src/main/java/ru/android4life/habittracker/adapter/HabitNotesAdapter.java
@@ -5,65 +5,32 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.Locale;
 
 import ru.android4life.habittracker.R;
-import ru.android4life.habittracker.db.dataaccessobjects.HabitDAO;
+import ru.android4life.habittracker.activity.MainActivity;
 import ru.android4life.habittracker.db.dataaccessobjects.HabitScheduleDAO;
-import ru.android4life.habittracker.db.tablesrepresentations.Habit;
 import ru.android4life.habittracker.db.tablesrepresentations.HabitSchedule;
-import ru.android4life.habittracker.utils.StringConstants;
 
 
 public class HabitNotesAdapter extends RecyclerView.Adapter<HabitNotesAdapter.ViewHolder> {
 
-    private List<String> mDataset;
+    private List<HabitSchedule> mDataset;
+    private Context context;
 
     public HabitNotesAdapter(int habitId, Context context) {
+        this.context = context;
         HabitScheduleDAO habitScheduleDAO = new HabitScheduleDAO(context);
-        HabitDAO habitDAO = new HabitDAO(context);
         List<HabitSchedule> schedules = habitScheduleDAO.findByHabitIdSortedByDateInDescendingOrder(habitId);
         mDataset = new ArrayList<>();
-        SimpleDateFormat dateFormatDaysAndMonthNumbers =
-                new SimpleDateFormat("dd.MM", Locale.ENGLISH);
         for (HabitSchedule schedule : schedules) {
-            Habit habit = (Habit) habitDAO.findById(schedule.getHabitId());
-            Calendar calendar = new GregorianCalendar();
-            Date currentDate = calendar.getTime();
-            calendar.add(Calendar.DATE, 1);
-            calendar.set(Calendar.HOUR_OF_DAY, 0);
-            calendar.set(Calendar.MINUTE, 0);
-            calendar.set(Calendar.SECOND, 0);
-            calendar.set(Calendar.MILLISECOND, 0);
-            Date tomorrow = calendar.getTime();
-            calendar.setTime(schedule.getDatetime());
-            calendar.add(Calendar.MINUTE, habit.getConfirmAfterMinutes());
-            Date habitsPerformanceConfirmationTime = calendar.getTime();
-
-            // if schedule is done or skipped its planned confirmation time passed
-            if (schedule.getDatetime().before(tomorrow) &&
-                    (schedule.isDone() != null || habitsPerformanceConfirmationTime.before(currentDate))) {
-                String historyNote = context.getString(R.string.two_subsequent_strings,
-                        dateFormatDaysAndMonthNumbers.format(schedule.getDatetime()),
-                        StringConstants.SPACE);
-                if (schedule.isDone() != null && schedule.isDone() && schedule.getNote() != null &&
-                        !schedule.getNote().equals(StringConstants.EMPTY_STRING))
-                    historyNote += context.getString(R.string.habit_was_performed_and_note,
-                            schedule.getNote());
-                else if (schedule.isDone() != null && schedule.isDone())
-                    historyNote += context.getString(R.string.habit_was_performed);
-                else
-                    historyNote += context.getString(R.string.habit_was_skipped);
-
-                mDataset.add(historyNote);
+            if (schedule.isDone() != null && !schedule.getNote().isEmpty()) {
+                mDataset.add(schedule);
             }
         }
     }
@@ -72,13 +39,21 @@ public class HabitNotesAdapter extends RecyclerView.Adapter<HabitNotesAdapter.Vi
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.note_text_view, parent, false);
-
         return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mTextView.setText(mDataset.get(position));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM", MainActivity.locale);
+        String date = dateFormat.format(mDataset.get(position).getDatetime());
+        holder.date.setText(date);
+        holder.name.setText(mDataset.get(position).getNote());
+        if (mDataset.get(position).isDone()) {
+            holder.name.setText(R.string.was_done);
+        } else {
+            holder.name.setText(R.string.was_skipped);
+        }
+        holder.name.setText(holder.name.getText() + " " +  mDataset.get(position).getNote());
     }
 
 
@@ -88,11 +63,15 @@ public class HabitNotesAdapter extends RecyclerView.Adapter<HabitNotesAdapter.Vi
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mTextView;
+        public TextView date;
+        public TextView name;
+        public RelativeLayout layout;
 
         public ViewHolder(View v) {
             super(v);
-            mTextView = (TextView) itemView.findViewById(R.id.note_name);
+            date = (TextView) itemView.findViewById(R.id.note_date);
+            name = (TextView) itemView.findViewById(R.id.note_name);
+            layout = (RelativeLayout) v;
         }
     }
 }
